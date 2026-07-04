49c57b3467858b9acf3ee6af9d95ba791a22f8eb
Thibault Pouch <thibault.pouch@epitech.eu>
2026-07-04 19:59:47 +0200

    feat(ui): cargo terminal design tokens + fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,9 @@
+import '@fontsource/oswald/500.css';
+import '@fontsource/oswald/700.css';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/700.css';
+import '@fontsource/barlow/400.css';
+import '@fontsource/barlow/600.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
